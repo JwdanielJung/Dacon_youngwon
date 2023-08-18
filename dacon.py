e423f36d6f0356ff085b0c43339d6488820aac74
JwdanielJung <wjdwodnjs302@snu.ac.kr>
2023-08-18 21:22:08 +0900

    modify files

--- dacon.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-if __name__ == "__main__":
-    train_df = pd.read_csv('./Dacon project/train.csv') 
-    test_df = pd.read_csv('./Dacon project/test.csv')
-    build_df = pd.read_csv('./Dacon project/building_info.csv')
-    sample_df = pd.read_csv('./Dacon project/sample_submission.csv')
